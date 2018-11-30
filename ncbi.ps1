0bfb7e6e87a4a9e70e32eea6c815d552976b77c8
RichardDW <richard.dewolff@gmail.com>
2018-11-30 14:49:13 +0100

    functionalized

--- ncbi.ps1
+++ ncbi.ps1
@@ -3,63 +3,80 @@
    This program will read a text file with SRA numbers to download
    from the NCBI website https://www.ncbi.nlm.nih.gov/
 .DESCRIPTION
-   This program downloads SRA files from the NCBI website 
+   This program downloads SRA files from the NCBI website
+   To get a file with the content to download goto NCBI site with a project code; 
+   https://www.ncbi.nlm.nih.gov/sra/?term=PRJNA353001
+   https://www.ncbi.nlm.nih.gov/sra/?term=PRJNA397453
+   and select "Send results to Run Selector and download Accession List
+   Files will, by default, be placed in C:\Users\<username>\ncbi\public\sra
+   This can be changed by running the bin\vdb-config 
+   #################################################################################
+   Prerequisites:
+   SRA Toolkit - https://trace.ncbi.nlm.nih.gov/Traces/sra/sra.cgi?view=software
+   IBM Aspera Connect Client - https://downloads.asperasoft.com/connect2/
 .PARAMETER <paramName>
    <Description of script parameter>
 .EXAMPLE
    <An example of using the script>
 #>
 
+# $srrFile contains the location of the the SRA Accession list
+[string] $srrFile = "C:\sra\SRR_Acc_List.txt"
 
-#################################################################################
-# Prerequisites:
-# SRA Toolkit - https://trace.ncbi.nlm.nih.gov/Traces/sra/sra.cgi?view=software
-# IBM Aspera Connect Client - 
-
-#
-# get list with the requested downloads
-# goto NCBI site with a project code; https://www.ncbi.nlm.nih.gov/sra/?term=PRJNA353001
-# and select "Send results to Run Selector and download Accession List
-$txtFile = "C:\sra\failed_downloads.txt"
-
-# Executable to run
+# Executable to run from the SRA Toolkit
 # prefect will get the files in SRA format
 # fastq-dump will convert SRA file to FASTQ format
-$fileExe = "C:\sra\sratoolkit\bin\prefetch.exe"
+[string] $fileExe = "C:\sra\sratoolkit\bin\prefetch.exe"
+[string] $maxDLsize = "999G"
+[string] $ascpArg = "c:\Program Files (x86)\Aspera\Aspera Connect\bin\ascp.exe|c:\Program Files (x86)\Aspera\Aspera Connect\etc\asperaweb_id_dsa.openssh"
 
-# get total number of items
-$total_lines = Get-Content $txtFile | Measure-Object –Line |Select-Object -ExpandProperty Lines
+function GetDownload {            
 
-$processed_lines = 0
+   [cmdletbinding()]            
+   param(            
+      [parameter(valuefrompipelinebypropertyname=$true)]            
+      [string]$ComputerName = $env:computername            
+   )            
+      
+   begin {}            
+   process {
+      # get total number of items 
+      [string] $total_lines = Get-Content $srrFile | Measure-Object –Line |Select-Object -ExpandProperty Lines
 
-foreach($line in [System.IO.File]::ReadLines($txtFile))
-{
-    $processed_lines = $processed_lines + 1
-    Write-Host "Now processing $processed_lines of $total_lines"   
-    & $fileExe -X 999G -a "C:\Users\User\AppData\Local\Programs\Aspera\Aspera Connect\bin\ascp.exe|C:\Users\User\AppData\Local\Programs\Aspera\Aspera Connect\etc\asperaweb_id_dsa.openssh" $line
-	#& $fileExe -O E:\PRJNA397453 $line
+      [int] $processed_lines = 0
+         
+      #foreach($line in [System.IO.File]::ReadLines($srrFile))
+      foreach($line in (Get-Content $srrFile)) {
+         $processed_lines = $processed_lines + 1
+         Write-Host "Now processing $processed_lines of $total_lines"   
+         & $fileExe -X $maxDLsize -a $ascpArg $line
+      }      
+   }            
+   end {}            
 }
 
-# -a "C:\Users\User\AppData\Local\Programs\Aspera\Aspera Connect\bin\ascp.exe|C:\Users\User\AppData\Local\Programs\Aspera\Aspera Connect\etc\asperaweb_id_dsa.putty"
+function CheckDownload {
+   # Count no of lines in download file
+   [string] $lines_infile_number = (Get-Content $srrFile | Measure-Object).Count
+   [string] $lines_infile = Get-Content $srrFile |Sort-Object
+   # retrieve directory list and count no of files
+   $lines_indir_number = (Get-ChildItem -Path E:\ncbi\public\sra -File |Measure-Object).Count
+   $lines_indir = Get-ChildItem -Path E:\ncbi\public\sra -File |Select-Object -ExpandProperty Basename |Sort-Object
+   # if not equal sort both lists and print differences
+   # check if numers are equal
+   If ($lines_infile_number -eq $lines_indir_number) {
+      Write-Host "Files are equal"
+   }
+   Else {
+      Write-Host "Not equal!!, files download $lines_indir_number and files is list $lines_infile_number"
+      Write-Host "Written a failed_downloads file in in to retrieve failed downloads"
+      Compare-Object $lines_infile $lines_indir | Select-Object -ExpandProperty InputObject | Out-File "C:\sra\failed_downloads.txt"
+   }
+}
 
-# get list with the requested downloads
-# goto NCBI site with a project code; https://www.ncbi.nlm.nih.gov/sra/?term=PRJNA353001
-# and select "Send results to Run Selector and download Accession List
-$txtFile = "C:\sra\PRJNA397453_SRR_Acc_List.txt"
+function Main {
+   GetDownload
+   CheckDownload  
+}
 
-# count no of lines in download file
-$lines_infile_number = (Get-Content $txtFile | Measure-Object).Count
-$lines_infile = Get-Content $txtFile |Sort-Object
-# retrieve directory list and count no of files
-$lines_indir_number = (Get-ChildItem -Path E:\ncbi\public\sra -File |Measure-Object).Count
-$lines_indir = Get-ChildItem -Path E:\ncbi\public\sra -File |Select-Object -ExpandProperty Basename |Sort-Object
-# if not equal sort both lists and print differences
-# check if numers are equal
-If ($lines_infile_number -eq $lines_indir_number) {
-    Write-Host "Files are equal"
-    }
-Else {
-    Write-Host "Not equal!!, files download $lines_indir_number and files is list $lines_infile_number"
-    Write-Host "Written a failed_downloads file in in to retrieve failed downloads"
-    Compare-Object $lines_infile $lines_indir | Select-Object -ExpandProperty InputObject |Out-File "C:\sra\failed_downloads.txt"
-    }
+Main
