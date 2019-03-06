065410bd803e048b53d59ac6fa250d2eeb96ad1a
RichardDW <richard.dewolff@gmail.com>
2019-03-06 10:14:52 +0100

    Azure updates

---
+++ azure/nsg-asg.ps1
@@ -0,0 +1,158 @@
+﻿<#
+Network Security Groups and Application Security Groups
+Tim Warner (timothy-warner@pluralsight.com)
+
+Reference: https://docs.microsoft.com/en-us/azure/virtual-network/tutorial-filter-network-traffic-powershell
+#>
+
+# Authenticate to Azure
+Connect-AzAccount
+
+# Create a resource group
+New-AzResourceGroup -ResourceGroupName myResourceGroup -Location 'West Europe'
+
+# Create two ASGs
+$webAsg = New-AzApplicationSecurityGroup `
+  -ResourceGroupName myResourceGroup `
+  -Name myAsgWebServers `
+  -Location 'West Europe'
+
+$mgmtAsg = New-AzApplicationSecurityGroup `
+  -ResourceGroupName myResourceGroup `
+  -Name myAsgMgmtServers `
+  -Location 'West Europe'
+
+  # Create two rules
+  $webRule = New-AzNetworkSecurityRuleConfig `
+  -Name "Allow-Web-All" `
+  -Access Allow `
+  -Protocol Tcp `
+  -Direction Inbound `
+  -Priority 100 `
+  -SourceAddressPrefix Internet `
+  -SourcePortRange * `
+  -DestinationApplicationSecurityGroupId $webAsg.id `
+  -DestinationPortRange 80,443
+
+$mgmtRule = New-AzNetworkSecurityRuleConfig `
+  -Name "Allow-RDP-All" `
+  -Access Allow `
+  -Protocol Tcp `
+  -Direction Inbound `
+  -Priority 110 `
+  -SourceAddressPrefix Internet `
+  -SourcePortRange * `
+  -DestinationApplicationSecurityGroupId $mgmtAsg.id `
+  -DestinationPortRange 3389
+
+  # Create a network security group (NSG) that includes ASG rules
+  $nsg = New-AzNetworkSecurityGroup `
+  -ResourceGroupName myResourceGroup `
+  -Location 'West Europe' `
+  -Name myNsg `
+  -SecurityRules $webRule,$mgmtRule
+
+  # Create a virtual network
+  $virtualNetwork = New-AzVirtualNetwork `
+  -ResourceGroupName myResourceGroup `
+  -Location 'West Europe' `
+  -Name myVirtualNetwork `
+  -AddressPrefix 10.0.0.0/16
+
+  # Add a subnet
+  Add-AzVirtualNetworkSubnetConfig `
+  -Name mySubnet `
+  -VirtualNetwork $virtualNetwork `
+  -AddressPrefix "10.0.2.0/24" `
+  -NetworkSecurityGroup $nsg
+$virtualNetwork | Set-AzVirtualNetwork
+
+# Get reference to VNet
+$virtualNetwork = Get-AzVirtualNetwork `
+ -Name myVirtualNetwork `
+ -Resourcegroupname myResourceGroup
+
+ # Create two public IP addresses
+ $publicIpWeb = New-AzPublicIpAddress `
+  -AllocationMethod Dynamic `
+  -ResourceGroupName myResourceGroup `
+  -Location 'West Europe' `
+  -Name myVmWeb
+
+$publicIpMgmt = New-AzPublicIpAddress `
+  -AllocationMethod Dynamic `
+  -ResourceGroupName myResourceGroup `
+  -Location 'West Europe' `
+  -Name myVmMgmt
+
+# Create two vNICs
+$webNic = New-AzNetworkInterface `
+  -Location 'West Europe' `
+  -Name myVmWeb `
+  -ResourceGroupName myResourceGroup `
+  -SubnetId $virtualNetwork.Subnets[0].Id `
+  -ApplicationSecurityGroupId $webAsg.Id `
+  -PublicIpAddressId $publicIpWeb.Id
+
+  $mgmtNic = New-AzNetworkInterface `
+  -Location 'West Europe' `
+  -Name myVmMgmt `
+  -ResourceGroupName myResourceGroup `
+  -SubnetId $virtualNetwork.Subnets[0].Id `
+  -ApplicationSecurityGroupId $mgmtAsg.Id `
+  -PublicIpAddressId $publicIpMgmt.Id
+
+  # Create two VMs
+  # Create user object
+$cred = Get-Credential -Message "Enter a username and password for the virtual machine."
+
+$webVmConfig = New-AzVMConfig `
+  -VMName myVmWeb `
+  -VMSize Standard_DS1_V2 | `
+Set-AzVMOperatingSystem -Windows `
+  -ComputerName myVmWeb `
+  -Credential $cred | `
+Set-AzVMSourceImage `
+  -PublisherName MicrosoftWindowsServer `
+  -Offer WindowsServer `
+  -Skus 2016-Datacenter `
+  -Version latest | `
+Add-AzVMNetworkInterface `
+  -Id $webNic.Id
+New-AzVM `
+  -ResourceGroupName myResourceGroup `
+  -Location 'West Europe' `
+  -VM $webVmConfig `
+  -AsJob
+
+  # Create user object
+$cred = Get-Credential -Message "Enter a username and password for the virtual machine."
+
+# Create the web server virtual machine configuration and virtual machine.
+$mgmtVmConfig = New-AzVMConfig `
+  -VMName myVmMgmt `
+  -VMSize Standard_DS1_V2 | `
+Set-AzVMOperatingSystem -Windows `
+  -ComputerName myVmMgmt `
+  -Credential $cred | `
+Set-AzVMSourceImage `
+  -PublisherName MicrosoftWindowsServer `
+  -Offer WindowsServer `
+  -Skus 2016-Datacenter `
+  -Version latest | `
+Add-AzVMNetworkInterface `
+  -Id $mgmtNic.Id
+New-AzVM `
+  -ResourceGroupName myResourceGroup `
+  -Location 'West Europe' `
+  -VM $mgmtVmConfig
+
+
+# Clean up
+Remove-AzResourceGroup -Name myResourceGroup -Force
+
+
+
+
+
+
