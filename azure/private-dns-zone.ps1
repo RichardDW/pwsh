d78a87e6f2bd00498ea03b923e529a2139824d6c
RichardDW <richard.dewolff@gmail.com>
2019-03-19 10:25:03 +0100

    updates

--- azure/private-dns-zone.ps1
+++ azure/private-dns-zone.ps1
@@ -4,14 +4,14 @@ New-NetFirewallRule -DisplayName 'Allow ICMPv4-In' -Protocol 'ICMPv4'
 Set-Item -Path WSMan:\localhost\Client\TrustedHosts -Value * -Force
 
 # Set variables
-$rgName = ''
-$zoneName = ''
-$vnet1Name = ''
-$host1Name = ''
-$host1IP = ''
-$vnet2Name = ''
-$host2Name = ''
-$host2IP = ''
+$rgName = 'plural-rg'
+$zoneName = 'weyland.local'
+$vnet1Name = 'prod-vnet1'
+$host1Name = 'winhost1'
+$host1IP = '10.10.10.4'
+$vnet2Name = 'prod-vnet2'
+$host2Name = 'winhost2'
+$host2IP = '10.20.20.4'
 
 $vnet1 = Get-AzVirtualNetwork -Name $vnet1Name -ResourceGroupName $rgName
 
