e597b405398c405d9e46d8ece0db756fe38f8c2b
unknown <richard@saturn.wolco.org>
2019-09-15 21:21:49 +0200

    Updates

--- scratch.ps1
+++ scratch.ps1
@@ -1,5 +1,6 @@
 ﻿Get-CimInstance
 
+#region Log
 Get-EventLog -LogName Security | Out-File c:\temp\events.log
 
 $myContents = Get-Content c:\temp\events.log 
@@ -14,12 +15,12 @@ $GroupedHash = $SortedHash | Group-Object {$_.filehash}
 $CountedHash = $GroupedHash | Sort-Object -Property Count
 $SortnCountedHash = $CountedHash | Select-Object -Property Count, Name | Sort-Object -Property Name 
 $SortnCountedHash 
-
+#endregion
 
 
 $UniqueHash = $fullCSV | Sort-Object -Property filehash -Unique | Export-Csv -Path 'C:\temp\2019 Script Audit Report Week 36- SORTED.csv'
 
-
+#region CSV
 
 $WeekReportPath = $WeekReportPath | Split-Path -Parent
 
@@ -27,14 +28,19 @@ foreach ($line in $SortnCountedHash) {
     Write-Output ("$line.count" + ";" + "$line.Name")
 }
 
-
-
 $fullCSV = Import-CSV -Path 'd:\temp\report.csv' -Delimiter ';' | Sort-Object -Property filehash
-$grouped = $fullCSV | Group-Object {$_.filehash} 
-$grouped | gm
+$grouped = $fullCSV | Group-Object {$_.filehash}
+$grouped 
+| Export-CSV -Path D:\temp\count.csv -Delimiter ';'
+$grouped | Out-File -FilePath d:\temp\group.txt 
+#endregion
 
-$grouped | Select-Object {$_.Count}, {$_.Name} | Export-CSV -Path D:\temp\count.csv -Delimiter ';'
+$grouped | Select-Object {$_.Count}, {$_.Name}, {$_.Group} | Export-CSV -Path D:\temp\count.csv -Delimiter ';'
 
 $uniqueSites = $fullCSV.Sitename | Sort-Object | Get-Unique
 
 $WeekReportPath = $WeekReportPath | Split-Path -Parent
+
+foreach ($key in $grouped.group.keys) {
+  write-host $grouped.group[$key]
+}
