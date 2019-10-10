5b5e83fc0f57612a46c67238f7744c7e3b365d32
RichardDW <richard.de.wolff@gmail.com>
2019-10-10 20:50:07 +0200

    updates

--- pwsh_masterclass.ps1
+++ pwsh_masterclass.ps1
@@ -18,7 +18,7 @@ $myprocs.GetType().FullName
 Get-Process | Select-Object -Property Name, @{name='procid';expression={$_.id}}
 
 Get-Process | Where-Object {$_.Handles -gt 1000}
-get-process | where Handles -gt 1000 | Sort-Object -Property Handles | Format-Table Name, Handles -AutoSize |gm
+get-process | Where-Object Handles -gt 1000 | Sort-Object -Property Handles | Format-Table Name, Handles -AutoSize |Get-Member
 # get process in gridview and kill the one's selected
 Get-Process | Out-GridView -PassThru | Stop-Process
 
