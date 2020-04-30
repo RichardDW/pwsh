bb6cdad47ab26b7ca4c3b46d38c555bc7784567d
RichardDW <richard.dewolff@gmail.com>
2020-04-30 21:16:57 +0200

    More pluralsight

---
+++ Untitled1.ps1
@@ -0,0 +1,18 @@
+﻿$myCIM = Get-CimInstance -ClassName Win32_Product 
+$myCIM | Where-Object Name -like "*Edge*"
+
+# select from Win32Reg_AddRemovePrograms
+
+# select from CIM_Datafile
+
+Get-CimInstance -ClassName Win32Reg_AddRemovePrograms
+
+Get-CimInstance -ClassName CIM_Datafile
+
+Get-WmiObject -Class Win32Reg_AddRemovePrograms
+
+Get-WmiObject -Class Win32_Product 
+
+$itemCount = 50
+
+Write-Output $itemcount
--- progs/PluralSight-Debugging_PWSH_with_VSCode/demo-01-singleFile.ps1
+++ progs/PluralSight-Debugging_PWSH_with_VSCode/demo-01-singleFile.ps1
@@ -1,5 +1,5 @@
 $admins = Get-LocalGroupMember administrators
 
 foreach ($admin in $admins) { 
-    Get-LocalUser -Name $admin.name
+    Get-LocalUser -SID $admin.SID
 }
